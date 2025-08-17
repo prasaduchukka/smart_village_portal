e7979ed397bf1739e1e91cdef49e2865f6100444
prasaduchukka <prasadchukka630@gmail.com>
2025-08-17 19:16:30 +0530

    17 commit

--- src/main/java/com/secure/controller/MeetingController.java
+++ src/main/java/com/secure/controller/MeetingController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.secure.entity.Complaint;
 import com.secure.entity.Meeting;
 import com.secure.service.MeetingService;
 
 @RestController
 @RequestMapping("/meetings")
+@CrossOrigin(origins = "*")
 public class MeetingController {
 	
 	@Autowired
